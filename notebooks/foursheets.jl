c2eb36f6c63d2ed16e332bacbdddf11edc1f7274
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2025-08-19 12:11:41 +0200

    update four sheet. Running

--- notebooks/foursheets.jl
+++ notebooks/foursheets.jl
@@ -7,16 +7,16 @@ using InteractiveUtils
 # ╔═╡ 2f4dd2f2-3c25-11ee-2f9d-8d445e1bd0c7
 # ╠═╡ show_logs = false
 begin
-	cd(joinpath(@__DIR__, ".."))
-	import Pkg
-	Pkg.activate(".")
-	Pkg.instantiate()
-	# 
-	using CairoMakie
-	using CairoMakie.Makie.PlotUtils
-	# 
-	using PlutoUI
-	using LaTeXStrings
+    cd(joinpath(@__DIR__, ".."))
+    using Pkg: Pkg
+    Pkg.activate(".")
+    Pkg.instantiate()
+    # 
+    using CairoMakie
+    using CairoMakie.Makie.PlotUtils
+    # 
+    using PlutoUI
+    using LaTeXStrings
 end
 
 # ╔═╡ 0c56f2a6-d146-4d73-afc4-ff6b4b00ca9e
@@ -33,7 +33,7 @@ See the [Review on Resonances](https://pdg.lbl.gov/2021/reviews/rpp2021-rev-reso
 (path = joinpath(@__DIR__, "..", "plots"); !isdir(path) && mkdir(path))
 
 # ╔═╡ 4a4f9272-b3d2-455e-8e1b-1598ab6bf241
-Base.sqrt(z,ϕ) = sqrt(z*cis(ϕ))*cis(-ϕ/2)
+sqrt_with_phase(z, ϕ) = sqrt(z * cis(ϕ)) * cis(-ϕ / 2)
 
 # ╔═╡ 3f823f5a-1e14-4e83-963b-ebbeeebb0fb4
 md"""
@@ -41,7 +41,7 @@ Defining the ω function for the complex energy plane.
 """
 
 # ╔═╡ 6bb69d21-4470-4cb3-88c7-556e3ff2afc7
-ω(ϵ) = sqrt(ϵ,-π) + sqrt(ϵ-1,-π)
+ω(ϵ) = sqrt_with_phase(ϵ, -π) + sqrt_with_phase(ϵ - 1, -π)
 
 # ╔═╡ ccbaf000-5f4c-446e-b5a8-6a963fc1795c
 md"""
@@ -50,9 +50,9 @@ Defining the ω functions for the different Riemann sheets.
 
 # ╔═╡ 46453bf3-770a-487a-bcf3-c4a34423a4f1
 begin
-	ω_II(ϵ) = -sqrt(ϵ,-π) + sqrt(ϵ-1,-π)
-	ω_III(ϵ) = sqrt(ϵ,-π) - sqrt(ϵ-1,-π)
-	ω_IV(ϵ) = -sqrt(ϵ,-π) - sqrt(ϵ-1,-π)
+    ω_II(ϵ) = -sqrt_with_phase(ϵ, -π) + sqrt_with_phase(ϵ - 1, -π)
+    ω_III(ϵ) = sqrt_with_phase(ϵ, -π) - sqrt_with_phase(ϵ - 1, -π)
+    ω_IV(ϵ) = -sqrt_with_phase(ϵ, -π) - sqrt_with_phase(ϵ - 1, -π)
 end
 
 # ╔═╡ f808b8b9-5dd3-48cc-959b-bbcab0f0d04c
@@ -61,14 +61,14 @@ Function to generate a semicircle in the complex plane for visualization purpose
 """
 
 # ╔═╡ ceee5325-c223-4b94-9ad0-e1359efd52b4
-function semicirc(c0,ch) 
-	vcat(
-		range(c0,2,101) .+ 1e-7im,
-		range(2,ch,11) .+ 1e-7im,
-		ch .* cis.(range(0,2π,20)[2:end-1]),
-		range(ch,2,11) .- 1e-7im,
-		range(2,c0,101) .- 1e-7im
-		)
+function semicirc(c0, ch)
+    vcat(
+        range(c0, 2, 101) .+ 1e-7im,
+        range(2, ch, 11) .+ 1e-7im,
+        ch .* cis.(range(0, 2π, 21)[2:20]),
+        range(ch, 2, 11) .- 1e-7im,
+        range(2, c0, 101) .- 1e-7im,
+    )
 end
 
 # ╔═╡ 9d3dae8f-d380-4254-a976-9d03cb8bf876
@@ -78,75 +78,72 @@ md"""
 
 # ╔═╡ 87c5322e-68b3-4fbf-9550-bdd7674bdcff
 function four_sheets_s_plane_3d(grid_position)
-	ym = 1.4
-	x = range(-1,2, 20)
-	y1 = range(-ym,-1e-5,10)
-	y2 = range(1e-5,ym, 10)
-	y = vcat(y1, y2)
-	# 
-	ax = Axis3(grid_position; aspect = (1, 1, 0.5),
-		    perspectiveness = 0.5, elevation = 0.47, azimuth=1.0,
-		    xlabel = L"\mathrm{Re}(s)",
-    		ylabel = L"\mathrm{Im}(s)",
-			zlabel = L"\mathrm{arb. units}",
-			zlabelrotation=π*(0.55),
-			xlabeloffset=20,
-			ylabeloffset=20,
-			# zlabeloffset=20,
-			xtickformat = xv->latexstring.(string.(xv)),
-			ytickformat = xv->latexstring.(string.(xv)),
-			ztickformat = xv->latexstring.(string.(xv))
-			)
-	# 
-	# hidezdecorations!(ax, ticks = false, ticklabels=true, label=false, grid=false)
-	# hidexdecorations!(ax, ticks = false, ticklabels=true, label=false, grid=false)
-	hidedecorations!(ax, ticks = true, ticklabels=true, label=false, grid=true)
-	# 
-	translate!(ax.scene, (0,0,2.5))
-	scale!(ax.scene, 1.2, 1.2, 1.2)
-	# 
-	function addsurface!(x,y, f; c=:green)
-		f′ = real ∘ f ∘ complex
-		z = -f′.(x,y')
-		surface!(ax, x, y, z;
-			transparency = false, colormap=PlotUtils.cgrad([c,c]))
-		wireframe!(ax, x, y, z, color=:black)
-	end
-	addsurface!(x,y1,ω_IV; c=:white)
-	addsurface!(x,y1,ω_II; c=:red)
-	addsurface!(x,y1,ω_III; c=:orange)
-	addsurface!(x,y1,ω; c=RGB(0.2,0.4,1))
-	addsurface!(x,y2,ω; c=RGB(0.2,0.4,1))
-	# 
-	xv = range(-1,0,2)
-	yv = zero.(xv)
-	zv = map(real ∘ ω, xv .- 1e-6im)
-	lines!(Point3f.(zip(xv,yv,zv .+ 0.03)), linewidth=5, color=:black)
-	# 
-	xv = range(0,2,101)
-	yv = zero.(xv)
-	zv = map(real ∘ ω, xv .- 1e-6im)
-	lines!(Point3f.(zip(xv,yv,zv .+ 0.03)), linewidth=5, color=:lime)
-	scatter!(Point3f[(0,0.03,0.03)], markersize=23, color=:lime)
-	# 
-	sv = ["21", "12", "11", "22"]
-	xy = Point3f[(0.8,-0.1,-0.2),(1.9,-0.1,1.2),(1.1,1.1,0.1),(1.9,-0.1,-1.5)]
-	scatter!(ax, xy, markersize=70, color=:white)
-	annotations!(sv, xy, fontsize=25, align=(:center,:center))
+    ym = 1.4
+    x = range(-1, 2, 20)
+    y1 = range(-ym, -1e-5, 10)
+    y2 = range(1e-5, ym, 10)
+    y = vcat(y1, y2)
+    # 
+    ax = Axis3(grid_position; aspect = (1, 1, 0.5),
+        perspectiveness = 0.5, elevation = 0.47, azimuth = 1.0,
+        xlabel = L"\mathrm{Re}(s)",
+        ylabel = L"\mathrm{Im}(s)",
+        zlabel = L"\mathrm{arb. units}",
+        zlabelrotation = π * (0.55),
+        xlabeloffset = 20,
+        ylabeloffset = 20,
+        # zlabeloffset=20,
+        xtickformat = xv -> latexstring.(string.(xv)),
+        ytickformat = xv -> latexstring.(string.(xv)),
+        ztickformat = xv -> latexstring.(string.(xv)),
+    )
+    hidedecorations!(ax, ticks = true, ticklabels = true, label = false, grid = true)
+    # 
+    # translate!(ax.scene, 0, 0, 2.5)
+    # scale!(ax.scene, 1.2, 1.2, 1.2)
+    # 
+    function addsurface!(x, y, f; c = :green)
+        f′ = real ∘ f ∘ complex
+        z = -f′.(x, y')
+        surface!(ax, x, y, z;
+            transparency = false, colormap = PlotUtils.cgrad([c, c]))
+        wireframe!(ax, x, y, z, color = :black)
+    end
+    addsurface!(x, y1, ω_IV; c = :white)
+    addsurface!(x, y1, ω_II; c = :red)
+    addsurface!(x, y1, ω_III; c = :orange)
+    addsurface!(x, y1, ω; c = RGB(0.2, 0.4, 1))
+    addsurface!(x, y2, ω; c = RGB(0.2, 0.4, 1))
+    # 
+    xv = range(-1, 0, 2)
+    yv = zero.(xv)
+    zv = map(real ∘ ω, xv .- 1e-6im)
+    lines!(Point3f.(zip(xv, yv, zv .+ 0.03)), linewidth = 5, color = :black)
+    # 
+    xv = range(0, 2, 101)
+    yv = zero.(xv)
+    zv = map(real ∘ ω, xv .- 1e-6im)
+    lines!(Point3f.(zip(xv, yv, zv .+ 0.03)), linewidth = 5, color = :lime)
+    scatter!(Point3f[(0, 0.03, 0.03)], markersize = 23, color = :lime)
+    # 
+    sv = ["II", "IV", "I", "III"]
+    xy = Point3f[(0.8, -0.1, -0.2), (1.9, -0.1, 1.2), (1.1, 1.1, 0.1), (1.9, -0.1, -1.5)]
+    scatter!(ax, xy, markersize = 60, color = :white)
+    text!(xy; text=sv, fontsize=25, align=(:center,:center))
 end
 
 # ╔═╡ 769b7491-4b29-4d42-b9e6-ad5cd0bb72e9
-begin
-	f = Figure(fontsize = 25, size = (800, 600))
-	four_sheets_s_plane_3d(f[1,1])
-	try 
-		save(joinpath(@__DIR__, "..", "plots", "foursheets_s_plane.pdf"), f)
-		save(joinpath(@__DIR__, "..", "plots", "foursheets_s_plane.png"), f)
-	catch e
-		@info "Could not save: $e.f"
-	end
-	# Box(f[1, 1], color = (:red, 0.2), strokewidth = 0)
-	f
+let
+    f = Figure(fontsize = 25, size = (800, 600))
+    four_sheets_s_plane_3d(f[1, 1])
+    try 
+    	save(joinpath(@__DIR__, "..", "plots", "foursheets_s_plane.pdf"), f)
+    	save(joinpath(@__DIR__, "..", "plots", "foursheets_s_plane.png"), f)
+    catch e
+    	@info "Could not save: $(e)"
+    end
+    # Box(f[1, 1], color = (:red, 0.2), strokewidth = 0)
+    f
 end
 
 # ╔═╡ 59e98547-1bb6-424c-9be7-362f8aee4ab0
@@ -156,50 +153,50 @@ md"""
 
 # ╔═╡ cd2215e2-2332-465d-a3e3-24b07b99ee8f
 function foursheets_omega_plane(grid_position)
-	ax = Axis(grid_position, aspect=1,
-		    xlabel = L"\mathrm{Re}(\omega)",
-    		ylabel = L"\mathrm{Im}(\omega)",
-			xtickformat = xv->latexstring.(string.(xv)),
-			ytickformat = xv->latexstring.(string.(xv)))
-	# plot(xlim=(-1.5,1.5),ylim=(-1.5,1.5), aspect_ratio=1)
-	poly!(ax, Point2f.(reim.(ω_II.(semicirc(0,1e5)))), color=:red)
-	poly!(ax, Point2f.(reim.(ω_III.(semicirc(0,1e5)))), color=:orange)
-	poly!(ax, Point2f.(reim.(ω_IV.(semicirc(0,2)))), color=RGB(0.95,0.95,0.95))
-	poly!(ax, Point2f.(reim.(ω.(semicirc(0,2)))), color=:blue)
-	# 
-	hlines!(ax, [0], linewidth=2, color=:black)
-	vlines!(ax, [0], linewidth=2, color=:black)
-	# 
-	lines!(ax, Point2f.(reim.(ω.(range(-1,0,2)))), linewidth=6, color=:black)
-	lines!(ax, Point2f.(reim.(ω.(range(0,2,61)))), linewidth=6, color=:lime)
-	scatter!(ax, Point2f[(0,1)], markersize=30, color=:lime)
-	# 
-	limits!(ax, (-1.5,1.5), (-1.5,1.5))
-	sv = ["21", "12", "11", "22"]
-	xy = Point2f[(0.4,0.4),(0.4,-0.4),(1.1,1.1),(1.1,-1.1)]
-	scatter!(ax, xy, markersize=90, color=:white)
-	annotations!(sv, xy, fontsize=30, align=(:center,:center))
+    ax = Axis(grid_position, aspect = 1,
+        xlabel = L"\mathrm{Re}(\omega)",
+        ylabel = L"\mathrm{Im}(\omega)",
+        xtickformat = xv -> latexstring.(string.(xv)),
+        ytickformat = xv -> latexstring.(string.(xv)))
+    # plot(xlim=(-1.5,1.5),ylim=(-1.5,1.5), aspect_ratio=1)
+    poly!(ax, Point2f.(reim.(ω_II.(semicirc(0, 50)))), color = :red)
+    poly!(ax, Point2f.(reim.(ω_III.(semicirc(0, 50)))), color = :orange)
+    poly!(ax, Point2f.(reim.(ω_IV.(semicirc(0, 2)))), color = RGB(0.95, 0.95, 0.95))
+    poly!(ax, Point2f.(reim.(ω.(semicirc(0, 2)))), color = :blue)
+    # 
+    hlines!(ax, [0], linewidth = 2, color = :black)
+    vlines!(ax, [0], linewidth = 2, color = :black)
+    # 
+    lines!(ax, Point2f.(reim.(ω.(range(-1, 0, 2)))), linewidth = 6, color = :black)
+    lines!(ax, Point2f.(reim.(ω.(range(0, 2, 61)))), linewidth = 6, color = :lime)
+    scatter!(ax, Point2f[(0, 1)], markersize = 30, color = :lime)
+    # 
+    limits!(ax, (-1.5, 1.5), (-1.5, 1.5))
+    sv = ["II", "III", "I", "IV"]
+    xy = Point2f[(0.4, 0.4), (1.1, -1.1), (1.1, 1.1), (0.4, -0.4)]
+    scatter!(ax, xy, markersize = 90, color = :white)
+    text!(xy; text=sv, fontsize=30, align=(:center,:center))
 end
 
 # ╔═╡ 95c399cc-09c3-46b2-a3d5-9d56592931b2
-let
-	f = Figure(fontsize = 25, size = (1300, 600))
-	four_sheets_s_plane_3d(f[1,1])
-	foursheets_omega_plane(f[1,2])
-	# 
-	# Box(f[1, 1], color = (:red, 0.2), strokewidth = 0)
-	# Box(f[1, 2], color = (:red, 0.2), strokewidth = 0)
-	# 
-	colsize!(f.layout, 1, Relative(0.62))
-	colgap!(f.layout, -30)
-	#
-	try
-		save(joinpath(@__DIR__, "..", "plots", "foursheets_maps.pdf"), f)
-		save(joinpath(@__DIR__, "..", "plots", "foursheets_maps.png"), f)
-	catch e
-		@info "Could not save: $e.f"
-	end
-	f
+begin
+    f = Figure(fontsize = 25, size = (1300, 600))
+    four_sheets_s_plane_3d(f[1, 1])
+    foursheets_omega_plane(f[1, 2])
+    # 
+    # Box(f[1, 1], color = (:red, 0.2), strokewidth = 0)
+    # Box(f[1, 2], color = (:red, 0.2), strokewidth = 0)
+    # 
+    colsize!(f.layout, 1, Relative(0.62))
+    colgap!(f.layout, -30)
+    #
+    try
+    	save(joinpath(@__DIR__, "..", "plots", "foursheets_maps.pdf"), f)
+    	save(joinpath(@__DIR__, "..", "plots", "foursheets_maps.png"), f)
+    catch e
+    	@info "Could not save: $(e)"
+    end
+    f
 end
 
 # ╔═╡ Cell order:
